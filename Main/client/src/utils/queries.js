c9ec92bcab02e0457b765200fd02ac7333889c95
DariaPressley <daria.mik@gmail.com>
2023-11-16 11:20:44 -0800

    added queries and mutations in utils

--- Main/client/src/utils/queries.js
+++ Main/client/src/utils/queries.js
@@ -1,22 +1,30 @@
-// import { gql } from '@apollo/client';
+import { gql } from '@apollo/client';
 
-// export const QUERY_TECH = gql`
-//   query tech {
-//     tech {
-//       _id
-//       name
-//     }
-//   }
-// `;
+export const QUERY_USER= gql`
 
-// export const QUERY_MATCHUPS = gql`
-//   query matchups($_id: String) {
-//     matchups(_id: $_id) {
-//       _id
-//       tech1
-//       tech2
-//       tech1_votes
-//       tech2_votes
-//     }
-//   }
+    query User {
+    users {
+      _id
+      email
+      password
+      username
+    }
+  }
+  
+`;
+
+export const QUERY_PRODUCT = gql`
+
+    query Product {
+    products {
+      _id
+      category
+      condition
+      description
+      ownerEmail
+      price
+      title
+    }
+  }
+  
 // `;
