3959d86acec4f5cbd4220a893c634d1dbea0f2c3
DariaPressley <daria.mik@gmail.com>
2023-11-16 13:50:01 -0800

    adding a not found page

--- Main/client/src/main.jsx
+++ Main/client/src/main.jsx
@@ -25,7 +25,7 @@ const router = createBrowserRouter([
                 element: <Login />
             },
             {
-                path: '/signip',
+                path: '/signup',
                 element: <Signup />
             },
             {
