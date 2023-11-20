0b8a403f2949e47aff6ab1487665d88fcbcfa364
DariaPressley <daria.mik@gmail.com>
2023-11-19 17:02:32 -0800

    adding homepage with css styling

--- Main/client/src/pages/Home.jsx
+++ Main/client/src/pages/Home.jsx
@@ -2,12 +2,15 @@ import { Link } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 import { QUERY_PRODUCT } from '../utils/queries';
 import Products from './Products';
+
 import {
   Breadcrumb,
   BreadcrumbItem,
   BreadcrumbLink,
   BreadcrumbSeparator,
-} from '@chakra-ui/react'
+} from '@chakra-ui/react';
+
+
 
 const Home = () => {
   const { data, loading } = useQuery(QUERY_PRODUCT, {
@@ -19,6 +22,7 @@ const Home = () => {
 
   return (
     <div>
+      <div class= "header">
       <Breadcrumb>
 
       <BreadcrumbItem isCurrentPage>
@@ -42,22 +46,20 @@ const Home = () => {
         </BreadcrumbItem>
 
       </Breadcrumb>
-
-      <div>
-        <h1> Welcome to GiftGenius </h1>
-        <p> Reappropriate your thoughtless gift here. </p>
-        <p> Buy or trade those unwanted gifts from your parents and friends! </p>
       </div>
-      {/* <div>
+      <h1> GiftGenius </h1>
+      <div class= "about">
+        <p> Don't throw away that sweater your parents gave you... </p>
+      </div>
+      <div class= "buttons">
         <Link to="/signup">
           <button className="btn btn-lg btn-danger">Create Account!</button>
         </Link>
         <Link to="/login">
           <button className="btn btn-lg btn-danger">Login!</button>
         </Link>
-      </div> */}
-      <div>
-        <h2> Regift that gift! </h2>
+      </div>
+      <div class= "presentDisplay">
         {loading ? (
           <div> loading... </div>
         ) : (
