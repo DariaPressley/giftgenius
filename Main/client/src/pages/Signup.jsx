a2eac6553784463a04eca316e1090f3e501de52d
Meline Sarkissian <sarkissian.mell321@yahoo.com>
2023-11-20 16:55:26 -0800

    added CSS styling

--- Main/client/src/pages/Signup.jsx
+++ Main/client/src/pages/Signup.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { ADD_PROFILE } from '../utils/mutations';
 import Auth from '../utils/auth';
+import './Signup.css'; 
 
 const Signup = () => {
   const [formState, setFormState] = useState({
